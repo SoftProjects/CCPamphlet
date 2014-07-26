01132515b20a004c74803613e1b83280625bafa7
shiyinayuriko <whitecometliliwill@gmail.com>
2014-07-27 04:08:01 +0800

    分离版本库

--- src/com/comicon/pamphlet/data/model/ItemModel.java
+++
@@ -1,7 +0,0 @@
-package com.comicon.pamphlet.data.model;
-
-import java.io.Serializable;
-
-public interface ItemModel extends Serializable{
-	public CircleModel getCircle();
-}
--- src/com/comicon/pamphlet/data/model/WorkModel.java
+++
@@ -1,9 +0,0 @@
-package com.comicon.pamphlet.data.model;
-
-public interface WorkModel extends ItemModel{
-	public String getName();
-	public String getPrice();
-	public String getCategory();
-	public String getTheme();
-	public String getSample();
-}
